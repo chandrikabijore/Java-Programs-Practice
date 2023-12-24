4d78c47feba6997327d95c6543d6fd02701edfd7
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2023-12-24 18:20:13 +0530

    Area_of_circle.java - 24/12/23

---
+++ 0003 - Area_of_circle.java
@@ -0,0 +1,14 @@
+// Program to ine area o circle..
+
+package com.chandrika;
+
+public class Area_of_circle
+{
+    public static void main(String[] args)
+    {
+        double Radius = 50.0;
+        double PI = 3.14159;
+        double Area = PI * Radius * Radius;
+        System.out.println("Area of circle is : " + Area);
+    }
+}
