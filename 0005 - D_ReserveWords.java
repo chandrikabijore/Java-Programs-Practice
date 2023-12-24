44ef1b0899fd444c1c9511b1ad3d9460a72ddf72
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2023-12-24 18:22:27 +0530

    D_ReserveWords.java - 24/12/23

---
+++ 0005 - D_ReserveWords.java
@@ -0,0 +1,16 @@
+package com.chandrika;
+
+public class D_ReserveWords
+{
+    public static void main(String[] args)
+    {
+        int x = 10;
+       // int if = 20;  // we can not use rw as identifier
+
+        int String = 888;
+        System.out.println(String);
+
+        int Runnable = 999;
+        System.out.println(Runnable);
+    }
+}
