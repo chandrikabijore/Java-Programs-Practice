5a7e659a683e5212e6d31f87cfc47430bfbd578b
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2023-12-24 18:18:59 +0530

    D_Identifier.java - 24/12/23

---
+++ 0002 - D_Identifier.java
@@ -0,0 +1,14 @@
+package com.chandrika;
+
+public class D_Identifier
+{
+    public static void main(String[] args)
+    {
+        int number = 10;
+        int Number = 20;
+        int NUMBER = 30;
+        int sum = number + Number + NUMBER;
+        System.out.println(number + " " + Number + " " + NUMBER);
+        System.out.println("Sum of this no is : " + sum );
+    }
+}
